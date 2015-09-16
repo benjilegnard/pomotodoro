18d9f8c6600caaa2ffe8955e5269e91cc4dddbfb
Benjamin Legrand <jibhaine@gmail.com>
2015-09-16 18:29:32 +0200

    Initial state : not so much yet.

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp')
+  , gutil = require('gulp-util')
+  , less = require('gulp-less')
+  , minifyCss = require('gulp-minify-css')
+  , sourcemaps = require('gulp-sourcemaps')
+  , b = require('browserify')
+;
+
+var config = {
+	baseDir:'./app',
+	less:{src:'/less/*',dest:'/css'},
+	bro:{src:'/js/index.js',dest:'/js/bundle.js'}
+};
+
+gulp.task('less',function(){
+	return gulp.src(config.baseDir + '/less/*.less')
+	.pipe(less())
+	.pipe(minifyCss())
+	.pipe(gulp.dest(config.baseDir + '/css'));
+});
+
+gulp.task('default',['less']);
